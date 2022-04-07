fe972f2d531796f7f4c8b960d51ce006232b32a1
Brandan Teng <64852466+brandanteng@users.noreply.github.com>
2022-04-07 16:21:35 +0800

    Updated for 2021 and 2022

--- js/bubblechart.js
+++ js/bubblechart.js
@@ -194,7 +194,7 @@
  var barChartWidth = countryDetailsWidth;
  var barChartHeight = countryDetailsHeight / 1.8;
  
- var years = [2015, 2016, 2017, 2018, 2019, 2020];
+ var years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
  
  // Color mapping by region
  var regionColors = {'America': '#49f47f', 'Europe': '#f44949', 'Africa': '#49c9f4', 'Asia/ Oceania': '#f4b642'};
